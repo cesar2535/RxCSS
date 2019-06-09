9a3c08bd2ca4f6e956730592813db4e2a7edafc5
Cesar Chen <cesar6134@gmail.com>
2019-06-09 17:52:00 +0800

    feat(package): update packages

--- src/observable.js
+++ src/observable.js
@@ -1,5 +1,5 @@
 import { of, merge } from 'rxjs';
-import {  scan, map } from 'rxjs/operators';
+import { scan, map } from 'rxjs/operators';
 
 const Observable = window.rxjs ? rxjs.Observable : require('rxjs').Observable;
 
